bb629402eaf24273ebd30a655d517c83b69cf4de
Douglas Sabino <douglasbsabino@hotmail.com>
2022-08-27 02:03:27 -0300

    concerta bug rota /DELETE /restaurants

--- migrations.sql
+++ migrations.sql
@@ -15,14 +15,6 @@ CREATE TABLE products(
   name VARCHAR(50) NOT NULL,
   price INTEGER NOT NULL,
   category VARCHAR(50) NOT NULL,
-  id_restaurants INTEGER NOT NULL,
-  FOREIGN KEY (id_restaurants) REFERENCES restaurants (id) ON DELETE CASCADE
+  id_restaurant INTEGER NOT NULL,
+  FOREIGN KEY fk_restaurant (id_restaurant) REFERENCES goomer.restaurants (id) ON DELETE CASCADE
 );
-
-insert into products (photo, name, price, category, id_restaurants) values ("https://github.com/goomerdev/job-dev-backend-interview/raw/master/media/logo-azul.png", "coca-cola",12, "bebidas",2)
-
-insert into products (photo, name, price, category, id_restaurants) values ("https://t1.rg.ltmcdn.com/pt/posts/9/4/3/bolo_comum_de_liquidificador_10349_600.jpg", "bolo de laranja",25, "doces",1);
-
-insert into products (photo, name, price, category, id_restaurants) values ("https://super.abril.com.br/wp-content/uploads/2019/09/si_408_oraculo_chiclete.png?quality=90&strip=info&resize=850,567", "chiclete",7, "doces",2);
-
-insert into products (photo, name, price, category, id_restaurants) values ("https://espacoliving.com/wp-content/uploads/2019/01/escova-de-dentes.jpg", "espova de dentes",3, "higiene",3);
--- seeds.sql
+++ seeds.sql
@@ -12,18 +12,26 @@ INSERT INTO restaurants (photo, name, addres) VALUES (
 );
 
 /* INSERÇÕES NA TABELA PRODUCTS */
-INSERT INTO products (photo, name, price, category, id_restaurants) VALUES (
-  "https://github.com/goomerdev/job-dev-backend-interview/raw/master/media/logo-azul.png", "coca-cola",12, "bebidas",2
+INSERT INTO goomer.products (photo, name, price, category, id_restaurant) VALUES (
+  'https://github.com/goomerdev/job-dev-backend-interview/raw/master/media/logo-azul.png', 
+  'Torda de Chocolate',
+  3,
+  'doce',
+  2
 );
 
-INSERT INTO products (photo, name, price, category, id_restaurants) VALUES (
-  "https://t1.rg.ltmcdn.com/pt/posts/9/4/3/bolo_comum_de_liquidificador_10349_600.jpg", "bolo de laranja",25, "doces",1
+INSERT INTO goomer.products (photo, name, price, category, id_restaurant) VALUES (
+  'https://espacoliving.com/wp-content/uploads/2019/01/escova-de-dentes.jpg', 
+  'Torda de Chocolate',
+  3,
+  'doce',
+  1
 );
 
-INSERT INTO products (photo, name, price, category, id_restaurants) VALUES (
-  "https://super.abril.com.br/wp-content/uploads/2019/09/si_408_oraculo_chiclete.png?quality=90&strip=info&resize=850,567", "chiclete",7, "doces",2
-);
-
-INSERT INTO products (photo, name, price, category, id_restaurants) VALUES (
-  "https://espacoliving.com/wp-content/uploads/2019/01/escova-de-dentes.jpg", "espova de dentes",3, "higiene",3
+INSERT INTO goomer.products (photo, name, price, category, id_restaurant) VALUES (
+  'https://super.abril.com.br/wp-content/uploads/2019/09/si_408_oraculo_chiclete.png?quality=90&strip=info&resize=850,567', 
+  'chiclete',
+  5,
+  'candle',
+  3
 );
