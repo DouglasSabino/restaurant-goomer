3cacde7ce75e34aaab4f1262cfd4307ac990879e
Douglas Sabino <douglasbsabino@hotmail.com>
2022-08-27 14:53:05 -0300

    implementa validação da rota /POST /products com Joi

--- controllers/controllerProducts .js
+++ controllers/controllerProducts .js
@@ -1,4 +1,5 @@
 const { servicesProducts } = require('../services/servicesProducts');
+const { productValidation } = require('../middlewares/productsValidation'); 
 
 const controllerProducts = {
     /** @type {import('express').RequestParamHandler} */
@@ -7,9 +8,14 @@ const controllerProducts = {
         return res.status(200).json(products);
     },
     /** @type {import('express').RequestParamHandler} */
-    postProduct: async (req, res, _next) => {
-        await servicesProducts.postProduct(req.body);
-        return res.status(201).json('created product sucessfully');
+    postProduct: async (req, res, next) => {
+        try {
+          const body = await productValidation(req.body)
+          await servicesProducts.postProduct(body);
+          return res.status(201).json({ message: "created product sucessfully"});
+        } catch (error) {
+          next(error);  
+        }
     },
 };
 
---
+++ middlewares/productsValidation.js
@@ -0,0 +1,31 @@
+const Joi = require('joi');
+
+const productValidation = async (body) => {
+    const schema = Joi.object({
+        photo: Joi.string().required().messages({
+            'string.empty': '400|"photo" is required',
+            'any.required':  '400|"photo" is required'
+        }),
+        name: Joi.string().required().min(4).messages({
+            'string.min': '400|"name" require four (4) or more characters',
+            'string.empty': '400|"name" is required',
+            'any.required':  '400|"name" is required'
+        }),
+        price: Joi.number().required().messages({
+            'number.empty': '400|"price" is required',
+            'any.required':  '400|"price" is required'
+        }),
+        category: Joi.string().required().messages({
+            'string.empty': '400|"category" is required',
+            'any.required':  '400|"category" is required'
+        }),
+        id_restaurant: Joi.number().required().messages({
+            'number.empty': '400|"id_restaurant" is required',
+            'any.required':  '400|"id_restaurant" is required'
+        }),
+    });
+    const resp = await schema.validateAsync(body);
+    return resp;
+}
+
+module.exports = { productValidation }
