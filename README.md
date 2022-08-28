57cf4fbde87dd84a64a2620fc4343cc9e5f8248f
Douglas Sabino <douglasbsabino@hotmail.com>
2022-08-27 22:30:21 -0300

    termina README

---
+++ README.md
@@ -0,0 +1,22 @@
+# Restaurante Goomer
+#### O Projeto é um desafio* onde é construido um CRUD de um restaurante onde podemos: 
+* Listar todos os restaurantes
+* Cadastrar novos restaurantes
+* Listar os dados de um restaurante
+* Alterar os dados um restaurante
+* Excluir um restaurante
+* Listar todos os produtos de um restautante
+* Criar um produto de um restaurante
+* Alterar um produto de um restaurante
+* Excluir um produto de um restaurante
+
+#### todas as validações foram feitas com a biblioteca JOI, para falicitar a criação e a população do banco de dados necessario para o uso da API todas as querys necessarias estão respectivamentes nos arquivos 'migrations.sql' e 'seeds.sql'.
+
+# Tecnologias e ferramentas utilizadas
+* NodeJs
+* express
+* MSC 
+* JOI
+* dotenv
+
+#### desafio* = https://github.com/goomerdev/job-dev-backend-interview
--- seeds.sql
+++ seeds.sql
@@ -1,18 +1,18 @@
 /* INSERÇÕES NA TABELA DOS RESTAURANTES */
 INSERT INTO restaurants (photo, name, addres) VALUES (
-  'https://www.creativefabrica.com/pt/product/restaurant-logo-338/', 
+  'https://www.pracadopapa.com.br/wp-content/uploads/2022/01/Melhores-Restaurantes-de-Belo-Horizonte-1360x765.jpg', 
   'restaurant 1', 
   'Rua Serra da Prata 480' 
 );
 
 INSERT INTO restaurants (photo, name, addres) VALUES (
-  'https://www.creativefabrica.com/pt/product/minimalist-restaurant-logo-7/', 
+  'https://www.melhoresdestinos.com.br/wp-content/uploads/2021/07/melhores-restaurantes-mundo-2021-capa-1536x805.jpg', 
   'restaurant 2', 
   'Rua ibitiranga 318' 
 );
 
 INSERT INTO restaurants (photo, name, addres) VALUES (
-  'https://www.creativefabrica.com/pt/product/minimalist-restaurant-logo-7/', 
+  'https://viagemegastronomia.cnnbrasil.com.br/wp-content/uploads/sites/5/2021/06/botanikafe-butanta.jpg?w=876&h=484&crop=1', 
   'restaurant 3', 
   'Rua das Giestas 310' 
 );
@@ -25,15 +25,15 @@ INSERT INTO restaurants (photo, name, addres) VALUES (
 
 /* INSERÇÕES NA TABELA PRODUCTS */
 INSERT INTO goomer.products (photo, name, price, category, id_restaurant) VALUES (
-  'https://github.com/goomerdev/job-dev-backend-interview/raw/master/media/logo-azul.png', 
-  'Torda de Chocolate',
+  'https://img.itdg.com.br/tdg/images/recipes/000/173/560/167511/167511_original.jpg?mode=crop&width=710&height=400', 
+  'Torda DE limão',
   3,
   'doce',
   2
 );
 
 INSERT INTO goomer.products (photo, name, price, category, id_restaurant) VALUES (
-  'https://espacoliving.com/wp-content/uploads/2019/01/escova-de-dentes.jpg', 
+  'https://s2.glbimg.com/aKlGFJonIoKIyqBY8cn9oZtsFY0=/0x0:320x213/984x0/smart/filters:strip_icc()/s.glbimg.com/po/rc/media/2012/06/13/17/14/58/420/pave_crocante_de_doce_de_leite.jpg', 
   'Torda de Chocolate',
   3,
   'doce',
@@ -45,5 +45,13 @@ INSERT INTO goomer.products (photo, name, price, category, id_restaurant) VALUES
   'chiclete',
   5,
   'candle',
+  3
+);
+
+INSERT INTO goomer.products (photo, name, price, category, id_restaurant) VALUES (
+  'https://static.clubedaanamariabraga.com.br/wp-content/uploads/2021/04/frango-assado-em-pe-1024x576.jpg?x41527', 
+  'Frango',
+  15,
+  'pratos principais',
   3
 );
